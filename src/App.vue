73ae552c2fd05e0e5cba9322afa2486c87b6c53c
Ian Herrington <ianherri20@gmail.com>
2023-01-05 10:26:35 -0600

    edit gitignore exclude node modules

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
-<script>
+<script setup>
 import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
