ad3c1a625804af076c4003ded8fd5dcd1ea16099
Christopher Cali <chris@consensusnetworks.com>
2023-12-29 11:50:58 -0500

    Add basic AboutUs (founders only for now)

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,24 +1,22 @@
 <template>
-  <section class="who-we-are-section fade-in-on-scroll">
-    <h2 class="about-section-heading">Our Team</h2>
-    
-    <div class="team-member">
-      <div v-for="person in people" :key="person.name" class="person-container">
-        <a :href="person.github" target="_blank">
-          <span class="link" />
-        </a>
-        <img
-          class="image"
-          :src="require(`@/assets/${person.image}`)"
-          height="150"
-          width="150"
-        >
-        <h3 class="name">
-          {{ person.name }}
-        </h3>
-        <p class="description">
-          {{ person.des }}
-        </p>
+  <section class="who-we-are-section fade-in-on-scroll">    
+    <!-- Founders Sub-Section -->
+    <div class="founders-section">
+      <h3 class="sub-section-heading">Founders</h3>
+      <div class="founders">
+        <div v-for="person in founders" :key="person.name" class="person-container">
+          <img class="image" :src="require(`@/assets/${person.image}`)" alt="Profile Picture">
+          <h3 class="name">{{ person.name }}</h3>
+          <p class="description">{{ person.des }}</p>
+          <div class="social-links">
+            <a :href="person.linkedin" target="_blank">
+              <img class="icon" src="/tech-stack-images/linkedin.png">
+            </a>
+            <a :href="person.github" target="_blank" class="github-logo">
+              <img class="icon" src="/tech-stack-images/github.png">
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   </section>
@@ -31,7 +29,9 @@ const people = [
         image: "chris.jpg",
         github: "https://github.com/ccali11",
         linkedin: "https://www.linkedin.com/in/christophercali/",
-        des: "Chris is chief executive, full stack guru, and part time philosophizer.",
+        des: "Chris started his career in consulting for Booz Allen Hamilton where he would eventually serve as Chief of Staff. While there he led a team \
+         of engineers to build internal knowledge management tools. Since then he's launched multiple companies that have raised over $5M in funding and has\
+          been coding full stack for the better part of the last 10 years",
         founder: true
     },
     {
@@ -39,7 +39,9 @@ const people = [
         image: "shane.jpg",
         github: "https://shanejearley.github.io",
         linkedin: "https://www.linkedin.com/in/shanejearley/",
-        des: "Shane is chief technical genius, full stack guru, and part time philosophizer.",
+        des: "Shane has been a full stack developer for over 10 years. He has helped build prototypes for companies that have gone on to raise over $5M in funding.\
+         Shane is a true swiss army knife when it comes to software; he has demonstrated the ability to learn and implement bleeding edge technologies \
+         very quickly.",
         founder: true
     },
     {
@@ -47,7 +49,7 @@ const people = [
         image: "ian.png",
         github: "https://ianherri.github.io",
         linkedin: "https://www.linkedin.com/in/ian-herrington-16939693/",
-        des: "Ian is chief designer, front-end guru, and full time philosophizer.",
+        des: "",
         founder: false
     },
     // {
@@ -67,53 +69,91 @@ const people = [
     //     founder: false
     // }
 ]
+
+const founders = people.filter(person => person.founder)
+// const nonFounders = people.filter(person => !person.founder).slice(0, 4)  // Limiting to max 4 non-founders
 </script>
 
 <style scoped>
 .who-we-are-section {
-  max-width: 100%;
   background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0), #000000),
     url('../assets/example-section-bg.png');
   background-size: cover;
   background-position: top;
 }
 
-.team-member {
+.sub-section-heading {
+  font-size: 36px;
+  margin-top: 50px;
+  margin-bottom: 50px;
+}
+
+.founders-section {
+  margin-bottom: 20px;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.founders {
+  display: flex;
   flex-wrap: wrap;
-  justify-content: left;
-  width: 100%;
+  justify-content: center;
+  gap: 30px;
 }
 
 .person-container {
+    width: 400px;
+    min-width: 250px;
+    min-height: 400px;
     padding: 12px;
     display: flex;
     flex-direction: column;
-    justify-content: left;
-    align-items: left;
-    position: relative;
+    align-items: center;
     gap: 12px;
+    margin-bottom: 60px;
 }
-
-.name {
-    margin: 0px;
-    font-size: 24px;
-}
-
-.description {
-}
-
-/* .link {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    left: 0;
-    z-index: 1;
-} */
 
 .image {
   border-radius: 50%;
+  width: 150px;
+  height: 150px;
+}
+
+.name {
+    margin-top: 20px;
+    margin-bottom: 20px;
+    font-size: 32px;
+}
+
+.description {
+  word-break: break-word; /* Ensure long words don't cause overflow */
+}
+
+.social-links {
+  margin-top: auto; /* Pushes the social links to the bottom */
+  display: flex;
+  gap: 40px;
+  justify-content: center; 
+  cursor: pointer;
+}
+
+.icon {
+  height: 32px;
+  cursor: pointer;
+}
+
+.github-logo {
+}
+
+@media (max-width: 768px) {
+  .founders {
+    flex-direction: column; /* Stack the items vertically on smaller screens */
+  }
+
+  .person-container {
+    width: 100%; /* Full width on smaller screens */
+    max-width: 300px; /* Maximum width to maintain consistency */
+  }
 }
 </style>
