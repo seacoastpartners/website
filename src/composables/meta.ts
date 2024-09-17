eab25a4e47d948871177985ab542b11d248e4ee8
Shane Earley <shanejearley@gmail.com>
2024-09-17 12:40:59 -0400

    Add site description

--- src/composables/meta.ts
+++ src/composables/meta.ts
@@ -7,9 +7,12 @@ type MetaOptions = {
 
 export function useMeta(options: MetaOptions) {
     const title = `STP | ${options.page}`
+    const description = "Startup-experienced full-stack software engineers committed to high-quality, fast delivery. Based in the USA."
     return useSeoMeta({
         title,
         ogTitle: title,
+        description: description,
+        ogDescription: description,
         ogUrl: `https://seacoast.partners${options.path || ""}`,
         twitterCard: "summary_large_image"
     })
